c1bb3131e540f496235af15628b31b2f63a31aa4
GeraldMatarrita <geramatarr@gmail.com>
2024-04-29 01:22:29 -0600

    Update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -10,17 +11,22 @@ public class Main {
             System.err.println("An error occurred while writing to the file: " + e.getMessage());
         }
 
-        List<String> instructions = null;
-        try {
-            instructions = ReadFile.readLines("instructions.txt");
-        } catch (IOException e) {
-            System.err.println("An error occurred while reading the file: " + e.getMessage());
-        }
+        List<String> instructions = new ArrayList<>();
+//        try {
+//            instructions = ReadFile.readLines("instructions.txt");
+//        } catch (IOException e) {
+//            System.err.println("An error occurred while reading the file: " + e.getMessage());
+//        }
+        instructions.add("new(1,14200)");
+        instructions.add("new(2,4500)");
+        instructions.add("use(0)");
+        instructions.add("use(1)");
 
         MMU.execute(instructions);
         MMU.printRealMemory();
         MMU.printVirtualMemory();
         MMU.printSymbolTable();
         System.out.println("\nFragmentation: " + MMU.getFragmentation() + " KB");
+        System.out.println("Page faults: " + MMU.getPageFaults());
     }
 }
