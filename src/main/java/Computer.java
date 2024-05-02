6c95ae3198b77305b215f6453460dd5d70d5c23c
GeraldMatarrita <geramatarr@gmail.com>
2024-05-02 03:12:04 -0600

    Update Computer

--- src/main/java/Computer.java
+++ src/main/java/Computer.java
@@ -1,7 +1,9 @@
 public class Computer {
 
-    private static final Integer CORES = 1;
-    private static final Integer INSTRUCTIONS_PER_SECOND = 1;
-    private static final Integer DISK_ACCESS_TIME_SECONDS = 5;
-    private static final Integer PAGE_SIZE_KB = 4;
+    public static final Integer CORES = 1;
+    public static final Integer PAGE_HIT_TIME = 1; // Time to access a page in memory in seconds
+    public static final Integer PAGE_SIZE_KB = 4; // Size of a page in KB
+    public static final Integer DISK_ACCESS_TIME_SECONDS = 5; // Time to execute a disk access in seconds
+    public static final Integer MAX_RAM_KB = 15; // Max space for physical memory
+    public static final Integer KB = 1000; // 1KB = 1000 bytes
 }
